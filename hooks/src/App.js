c72f1456c584dec2dd3e53c4cbbe6d31684855b9
HadeeqaImran <hadeeqaimran786@gmail.com>
2024-05-16 15:39:03 +0500

    useState on Array

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Counter from './components/Counter';
 import Twoway_Counter from './components/Twoway_Counter';
 import Person from './components/Person';
+import Array from './components/Array';
+
 function App() {
   return (
     <div className="App">
       <Twoway_Counter />
       <Counter />
       <Person />
+      <Array />
     </div>
   );
 }

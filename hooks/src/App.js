ae703f4b078b97425d0bc9fc41669c438ce94443
HadeeqaImran <hadeeqaimran786@gmail.com>
2024-05-17 10:50:46 +0500

    useEffect dependency list

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -4,6 +4,7 @@ import Twoway_Counter from './components/Twoway_Counter';
 import Person from './components/Person';
 import Array from './components/Array';
 import MouseContainer from './components/MouseContainer';
+import IntervalCounter from './components/IntervalCounter'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Counter />
       <Person />
       <Array />
+      <IntervalCounter />
     </div>
   );
 }
